d8a583e59915ea5a2302f88244707b413ae2b40a
Branislav Vladisavljev <branebrvl@gmail.com>
2015-01-27 07:25:27 +0000

    pusher and twit added

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-var express = require('express');
-var app = express();
+var env = require('node-env-file');
+var Twit = require('twit');
+var Pusher = require('pusher');
 
-app.set('port', (process.env.PORT || 9000));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID
+  key: process.env.PUSHER_KEY
+  secret: process.env.PUSHER_SECRET
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+// Load any undefined ENV variables form a specified file.
+env(__dirname + '/.env');
+
+var Twitter = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
+
+var stream = Twitter.stream('statuses/filter', {
+  track: process.env.TRACK_FILTER
+});
+
+stream.on('tweet', function (data) {
+  var tweet = {
+    originalfeed_id: data.id,
+    name: data.user.name,
+    screen_name: data.user.screen_name,
+    created_at: data.user.created_at,
+    profile_image_url: data.user.profile_image_url,
+    created_at: data.created_at,
+    post: data.text,
+    url: 'https://twitter.com/' + data.user.screen_name + '/status/' + data.id_str
+  };
+
+  pusher.trigger('test_channel', 'my_event', {
+    "message": "hello world"
+  });
+
+  console.log(tweet);
+});
+// stream.on('error', function (data) {
+//   console.log(data);
+// });
+// stream.on('connect', function (data) {
+//   console.log(data);
+// });
+// stream.on('connected', function (data) {
+//   console.log(data);
+// });
