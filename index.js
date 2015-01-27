fa59e27350a188a8dca2914c1f78b2e82fd10aa4
Branislav Vladisavljev <branebrvl@gmail.com>
2015-01-27 10:40:04 +0000

    app know about prod vs local environmnent

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ var env = require('node-env-file');
 var Twit = require('twit');
 var Pusher = require('pusher');
 
+var detectEnvironment = process.env.ENV || 'local'
+// Load any undefined ENV variables form a specified file.
+if (detectEnvironment == 'local') {
+  env(__dirname + '/.env');
+}
+
 var pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID
-  key: process.env.PUSHER_KEY
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_KEY,
   secret: process.env.PUSHER_SECRET
 });
-
-// Load any undefined ENV variables form a specified file.
-env(__dirname + '/.env');
-
 var Twitter = new Twit({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -34,18 +36,7 @@ stream.on('tweet', function (data) {
     url: 'https://twitter.com/' + data.user.screen_name + '/status/' + data.id_str
   };
 
-  pusher.trigger('test_channel', 'my_event', {
-    "message": "hello world"
+  pusher.trigger('twitter_streaming_channel', 'hashtag_posted', {
+    "tweet": tweet
   });
-
-  console.log(tweet);
 });
-// stream.on('error', function (data) {
-//   console.log(data);
-// });
-// stream.on('connect', function (data) {
-//   console.log(data);
-// });
-// stream.on('connected', function (data) {
-//   console.log(data);
-// });
